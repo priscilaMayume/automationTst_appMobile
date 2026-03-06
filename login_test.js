b1643a2591c3d3ddd796adafa034dabd065bea8b
priscilaMayume <prisccila@gmail.com>
2026-03-06 10:07:05 -0300

    test login

--- login_test.js
+++ login_test.js
@@ -1,5 +1,23 @@
 Feature('login');
 
-Scenario('Login with ',  ({ I }) => {
+Scenario('Login with sucess',  ({ I }) => {
+    I.click("~email");
+    I.clearField("~email");
+    I.appendField("~email", "teste@teste.com");    
+    I.fillField('~senha','123456')
+    //click enter
+    I.tap('~entrar')
+    //check
+    I.waitForElement('~codigo', 3)
+    I.seeElement('~codigo')
+});
 
+Scenario('Login with error',  ({ I }) => {
+    I.fillField('~email','teste@error.com')
+    I.fillField('~senha','123456')
+    //click enter
+    I.tap('~entrar')
+    //check
+    I.waitForElement('~lognFail', 3)
+    I.seeElement('~lognFail')
 });
